0c9541f38d5c8e7ee50e3abee299f560e45ec510
James Lu <jamtlu@gmail.com>
2018-02-04 17:13:59 -0500

    Make Header not pure

--- src/views/Header/Header.js
+++ src/views/Header/Header.js
@@ -48,7 +48,7 @@ const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
 const TITLE_OFFSET = Platform.OS === 'ios' ? 70 : 56;
 
 type Props = HeaderProps & { isLandscape: boolean };
-export class UnwrappedHeader extends React.PureComponent<Props, State> {
+export class UnwrappedHeader extends React.Component {
   static defaultProps = {
     leftInterpolator: HeaderStyleInterpolator.forLeft,
     titleInterpolator: HeaderStyleInterpolator.forCenter,
